511e0332e69512257c79643e63e406a9be61d353
srujan-patel <srujanp98@gmail.com>
2023-03-22 13:21:09 -0600

    Configured the project structure

---
+++ Booking.Domain/Models/Hotel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Models
+{
+    public class Hotel
+    {
+
+        public int m_hotelID { get; set; }
+    }
+}
